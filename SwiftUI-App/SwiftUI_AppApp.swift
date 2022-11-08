e898479844a02c8d30e5f37f829faa77ae16c819
suryaharahap <suryaharahap18@gmail.com>
2022-11-08 13:13:04 +0700

    Initialize repository SwiftUI

---
+++ SwiftUI-App/SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppApp.swift
+//  SwiftUI-App
+//
+//  Created by Surya on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
